029ec9629c36de4f0842b9e67f12e8e9bf809309
Florian <floriandelafosse@gmail.com>
2024-10-28 14:01:57 +0100

    debut front header

--- frontend/IT_Training_front/src/App.jsx
+++ frontend/IT_Training_front/src/App.jsx
@@ -1,15 +1,16 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
 
 function App() {  
 
   return (
     <>
-    <p>test</p>
-      <>
+      <Navbar/>      
+      <main>
         <Outlet/>
-      </>
+      </main>
     </>
   )
 }

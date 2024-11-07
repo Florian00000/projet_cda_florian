7000128f45eff2a464ab8f01742a546999e2aa71
Florian <floriandelafosse@gmail.com>
2024-11-07 17:08:28 +0100

    controllers test-user

--- backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/testUser/TestUserAdminController.java
+++ backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/testUser/TestUserAdminController.java
@@ -2,11 +2,13 @@ package com.example.it_training_back.controller.testUser;
 
 import com.example.it_training_back.dto.testUser.TestUserDtoGet;
 import com.example.it_training_back.dto.testUser.TestUserDtoPost;
+import com.example.it_training_back.dto.training.TrainingDtoGet;
 import com.example.it_training_back.service.TestUserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -22,5 +24,15 @@ public class TestUserAdminController {
     @PostMapping("/test-user/add")
     public ResponseEntity<TestUserDtoGet> addTestUser(@RequestBody TestUserDtoPost testUserDtoPost) {
         return ResponseEntity.status(HttpStatus.CREATED).body(testUserService.addTestUser(testUserDtoPost));
+    }
+
+    @GetMapping("/test-user/get-all")
+    public ResponseEntity<List<TestUserDtoGet>> getAllTestUser() {
+        return ResponseEntity.status(HttpStatus.OK).body(testUserService.getAllTestUsers());
+    }
+
+    @PatchMapping("/test-user/{idTestUser}/test-training/{idTraining}")
+    public ResponseEntity<TrainingDtoGet> addTestToTraining(@PathVariable long idTestUser, @PathVariable int idTraining) {
+        return ResponseEntity.ok(testUserService.addTestToTraining(idTestUser, idTraining));
     }
 }
--- backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/testUser/TestUserVisitorController.java
+++ backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/testUser/TestUserVisitorController.java
@@ -1,6 +1,8 @@
 package com.example.it_training_back.controller.testUser;
 
 import com.example.it_training_back.dto.testUser.TestUserDtoGet;
+import com.example.it_training_back.dto.testUser.proposition.PropositionDtoGet;
+import com.example.it_training_back.dto.testUser.question.QuestionDtoGet;
 import com.example.it_training_back.service.TestUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,5 +23,15 @@ public class TestUserVisitorController {
     @GetMapping("/test-user/{id}")
     public ResponseEntity<TestUserDtoGet> getTestUser(@PathVariable long id) {
         return ResponseEntity.ok(testUserService.getTestUser(id));
+    }
+
+    @GetMapping("/test-user/question/{id}")
+    public ResponseEntity<QuestionDtoGet> getQuestion(@PathVariable long id) {
+        return ResponseEntity.ok(testUserService.getQuestion(id));
+    }
+
+    @GetMapping("/test-user/proposition/{id}")
+    public ResponseEntity<PropositionDtoGet> getProposition(@PathVariable long id) {
+        return ResponseEntity.ok(testUserService.getProposition(id));
     }
 }

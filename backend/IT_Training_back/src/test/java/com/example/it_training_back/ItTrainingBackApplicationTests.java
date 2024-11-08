5135294c7bd093991537e9a2fc6b8809962a14cf
Florian <floriandelafosse@gmail.com>
2024-11-08 12:15:36 +0100

    test intégration en commentaire

--- backend/IT_Training_back/src/test/java/com/example/it_training_back/ItTrainingBackApplicationTests.java
+++ backend/IT_Training_back/src/test/java/com/example/it_training_back/ItTrainingBackApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.it_training_back;
 
+import com.example.it_training_back.service.ThemeService;
+import com.example.it_training_back.service.TrainingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class ItTrainingBackApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//class ItTrainingBackApplicationTests {
+//
+//    @MockBean
+//    private TrainingService trainingService;
+//    private ThemeService themeService;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

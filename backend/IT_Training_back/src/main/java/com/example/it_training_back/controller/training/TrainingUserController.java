430f783744da2aa364df3ed10762299781039a18
Florian <floriandelafosse@gmail.com>
2024-12-09 16:07:40 +0100

    getSessionByUser pour dasboard perso

--- backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/training/TrainingUserController.java
+++ backend/IT_Training_back/src/main/java/com/example/it_training_back/controller/training/TrainingUserController.java
@@ -1,10 +1,13 @@
 package com.example.it_training_back.controller.training;
 
 import com.example.it_training_back.dto.BaseResponseDto;
+import com.example.it_training_back.dto.session.SessionDtoGet;
 import com.example.it_training_back.service.UserToTrainingService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -30,5 +33,10 @@ public class TrainingUserController {
         if (result){
             return ResponseEntity.ok(new BaseResponseDto("User has registered to Session", true));
         }else return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseResponseDto("User not registered to Session", false));
+    }
+
+    @GetMapping("/session/byUser/{idUser}")
+    public ResponseEntity<List<SessionDtoGet>> userHasRegisteredToSession(@PathVariable long idUser) {
+        return ResponseEntity.ok(userToTrainingService.getAllSessionByUserID(idUser));
     }
 }
